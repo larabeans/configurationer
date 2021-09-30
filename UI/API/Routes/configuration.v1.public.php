812bd43859fabcdfca3bbfb15a80823182c50bc6
Sajjad Naqvi <sajjad@orenda.systems>
2021-09-30 18:14:01 +0500

    updates api documentation

--- UI/API/Routes/configuration.v1.public.php
+++ UI/API/Routes/configuration.v1.public.php
@@ -5,16 +5,16 @@
  * @apiSuccessExample {json} Success-Response:
 HTTP/1.1 200 OK
 {
-"data":{
-"object":"Configuration",
-"id":"eqwja3vw94kzmxr0",
-"configuration": "{\"Site_Name\":\"www.ccc.com\",\"Language\":\"Urdu\",\"Currency\":\"PKR\"}" // should be json_decode
-},
-"meta":{
-"include":[
-],
-"custom":[
-]
-}
+    "data":{
+        "object":"Configuration",
+        "id":"eqwja3vw94kzmxr0",
+         "configuration": "{\"Site_Name\":\"www.ccc.com\",\"Language\":\"Urdu\",\"Currency\":\"PKR\"}" // should be json_decode
+    },
+    "meta":{
+        "include":[
+        ],
+        "custom":[
+        ]
+    }
 }*/
 
