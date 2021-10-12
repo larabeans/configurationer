114bb2f5bebb6bcfe84e20780d3e6e5ae0aca39b
Syed Ali Murtaza <ali@kazmi.me>
2021-10-12 16:54:32 +0500

    makes default config publically available

--- UI/API/Routes/GetDefaultConfiguration.v1.public.php
+++ UI/API/Routes/GetDefaultConfiguration.v1.public.php
@@ -24,7 +24,5 @@ use App\Containers\Vendor\Configurationer\UI\API\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('configurations', [Controller::class, 'defaultConfiguration'])
-    ->name('api_configuration_default_configuration')
-    ->middleware(['auth:api']);
-//dd("here");
+    ->name('api_configuration_default_configuration');
 
