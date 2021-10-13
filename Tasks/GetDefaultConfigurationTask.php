0681f292105befc3d1316f5fb94626c994bd803c
Sajjad Naqvi <sajjad@orenda.systems>
2021-10-13 14:35:47 +0500

    adds session and auth data

--- Tasks/GetDefaultConfigurationTask.php
+++ Tasks/GetDefaultConfigurationTask.php
@@ -4,6 +4,9 @@ namespace App\Containers\Vendor\Configurationer\Tasks;
 
 use App\Containers\Vendor\Configurationer\Data\Repositories\ConfigurationRepository;
 use App\Ship\Parents\Tasks\Task;
+use App\Containers\AppSection\Authentication\Tasks\GetAuthenticatedUserTask;
+use App\Containers\AppSection\Authorization\Tasks\GetAllPermissionsTask;
+use App\Containers\AppSection\Authorization\Tasks\FindRoleTask;
 
 class GetDefaultConfigurationTask extends Task
 {
@@ -16,6 +19,49 @@ class GetDefaultConfigurationTask extends Task
 
     public function run()
     {
-        return config('configuration.configuration');
+        $assignedPermissionData = [];
+        $allPermissionsData = [];
+        $auth = [];
+        $session = [];
+
+        $configData = config('configuration.configuration');
+        $user = app(GetAuthenticatedUserTask::class)->run();
+        $allPermissions = app(GetAllPermissionsTask::class)->run(true);
+
+
+        foreach ($allPermissions as $value) {
+            array_push($allPermissionsData, $value->name);
+        }
+
+        //checking if user has role, if it has fetch all the permission assign to role
+        if (sizeof($user->roles) == 0) {
+            $auth['granted_permissions'] = null;
+
+        } else {
+
+            $role = $user->roles[0]->id; //get role_id of assigned to user
+            $roleData = app(FindRoleTask::class)->run($role);
+            foreach ($roleData->permissions as $r) {
+                array_push($assignedPermissionData, $r->name);
+
+            }
+            $auth['granted_permissions'] = $assignedPermissionData;
+        }
+
+        $auth['all_permissions'] = $allPermissionsData;
+        $session['user_id'] = $user->id;
+
+        if ($user->tenant_id == null) {
+
+            $session['tenant_id'] = null;
+            $session['multi_tenancy_side'] = 2;
+        } else {
+            $session['tenant_id'] = $user->tenant_id;
+            $session['multi_tenancy_side'] = 1;
+
+        }
+        $response= array_merge($configData,['session'=>$session],['auth'=>$auth]);
+
+        return $response;
     }
 }
