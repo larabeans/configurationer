01e185b4ab3c276e29200ab341f7e2ee1e781214
Sajjad Naqvi <sajjad@orenda.systems>
2021-10-15 16:35:16 +0500

    Adds new route

---
+++ UI/API/Routes/GetConfigurationHistory.v1.public.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @apiGroup           Configurationer
+ * @apiName            getConfigurationHistory
+ *
+ * @api                {GET} /v1/history/configurations Get Configuration History
+ * @apiDescription     Show the history of changes in configuration
+ *
+ * @apiParam           {String} type user/tenant
+ *
+ * @apiVersion         1.0.0
+ * @apiPermission      Authenticated User
+ *
+ * @apiUse             ConfigurationSuccessSingleResponse
+ */
+
+use App\Containers\Vendor\Configurationer\UI\API\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+
+Route::get('history/{type}/configurations', [Controller::class, 'getConfigurationHistory'])
+    ->name('api_configurationer_get_configuration_history')
+    ->middleware(['auth:api']);
+
