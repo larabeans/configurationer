fba89d74cdd3e1bdc579b386331af04413bda897
Sajjad Naqvi <sajjad@orenda.systems>
2021-10-15 12:27:18 +0500

    adds new migration

---
+++ Data/Migrations/2021_10_15_054959_create_configuration_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateConfigurationHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('configuration_histories', function (Blueprint $table) {
+            if (config('uuider.installed', false)) {
+                $table->uuid('id')->primary('id');
+                $table->uuid('configuration_id');
+            } else {
+                $table->increments('id')->primary('id');
+                $table->foreignId('configuration_id');
+            }
+            $table->text('configuration');
+
+            $table->timestamps();
+            //$table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('configuration_histories');
+    }
+}
