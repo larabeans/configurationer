da9b7aaf6096573d5785ac15e3eff4352df2be2a
Sajjad Naqvi <sajjad@orenda.systems>
2021-10-15 12:28:32 +0500

    adds new model

---
+++ Models/ConfigurationHistory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Containers\Vendor\Configurationer\Models;
+
+use App\Containers\Vendor\Beaner\Parents\Models\Model;
+use App\Containers\Vendor\Configurationer\Models\Configuration;
+
+class ConfigurationHistory extends Model
+{
+    protected $table="configuration_histories";
+    protected $fillable = [
+        'configuration_id',
+        'configuration'
+    ];
+
+    protected $attributes = [
+
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    protected $casts = [
+
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * A resource key to be used in the serialized responses.
+     */
+    protected string $resourceKey = 'ConfigurationHistory';
+
+    public function Configuration(){
+        return $this->belongsTo(Configuration::class,"configuration_id");
+    }
+}
