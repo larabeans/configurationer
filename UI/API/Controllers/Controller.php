f3ac51b0f8cfbe57e88fe81651d3ffd580e31353
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-05 18:56:05 +0500

    updates controller add function for update host configuration

--- UI/API/Controllers/Controller.php
+++ UI/API/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Containers\Vendor\Configurationer\UI\API\Controllers;
 
 use App\Containers\Vendor\Configurationer\Actions\GetHostConfigurationAction;
+use App\Containers\Vendor\Configurationer\Actions\UpdateHostConfigurationAction;
 use App\Containers\Vendor\Configurationer\UI\API\Requests\CreateConfigurationRequest;
 use App\Containers\Vendor\Configurationer\UI\API\Requests\DeleteConfigurationRequest;
 use App\Containers\Vendor\Configurationer\UI\API\Requests\GetAllConfigurationsRequest;
@@ -12,6 +13,7 @@ use App\Containers\Vendor\Configurationer\UI\API\Requests\UpdateConfigurationReq
 use App\Containers\Vendor\Configurationer\UI\API\Requests\GetUserConfigurationRequest;
 use App\Containers\Vendor\Configurationer\UI\API\Requests\GetTenantConfigurationRequest;
 use App\Containers\Vendor\Configurationer\UI\API\Requests\GetConfigurationHistoryRequest;
+use App\Containers\Vendor\Configurationer\UI\API\Requests\UpdateHostConfigurationRequest;
 use App\Containers\Vendor\Configurationer\UI\API\Transformers\ConfigurationTransformer;
 use App\Containers\Vendor\Configurationer\UI\API\Requests\GetDefaultConfigurationRequest;
 use App\Containers\Vendor\Configurationer\Actions\CreateConfigurationAction;
@@ -90,6 +92,12 @@ class Controller extends ApiController
     public function updateConfiguration(UpdateConfigurationRequest $request)
     {
         $configuration = app(UpdateConfigurationAction::class)->run($request);
+        return $this->transform($configuration, ConfigurationTransformer::class);
+    }
+
+    public function updateHostConfiguration(UpdateHostConfigurationRequest $request)
+    {
+        $configuration = app(UpdateHostConfigurationAction::class)->run($request);
         return $this->transform($configuration, ConfigurationTransformer::class);
     }
 
