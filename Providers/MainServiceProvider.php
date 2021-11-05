ac9918e4aa7ed25b169b9ee26a25155d1af4adfa
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-05 10:25:07 +0500

    Register TenanterRegisterEvent and their listener

--- Providers/MainServiceProvider.php
+++ Providers/MainServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Containers\Vendor\Configurationer\Providers;
 
+use App\Containers\Vendor\Configurationer\Listeners\CreateTenantConfigurationListerner;
+
+use App\Containers\Vendor\Tenanter\Events\TenantRegisteredEvent;
 use App\Ship\Parents\Providers\MainProvider;
+use Illuminate\Support\Facades\Event;
 
 /**
  * The Main Service Provider of this container, it will be automatically registered in the framework.
@@ -37,5 +41,15 @@ class MainServiceProvider extends MainProvider
 
         // $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         // ...
+    }
+    public function boot(): void
+    {
+        parent::boot();
+
+        Event::listen(
+            TenantRegisteredEvent::class,
+            [CreateTenantConfigurationListerner::class, 'handle']
+        );
+
     }
 }
