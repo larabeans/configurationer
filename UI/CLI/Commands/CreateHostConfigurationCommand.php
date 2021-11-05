92d770069436caa238ff22b9640703bd8906469a
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-05 18:16:48 +0500

    Add command to make host configuration

---
+++ UI/CLI/Commands/CreateHostConfigurationCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Containers\Vendor\Configurationer\UI\CLI\Commands;
+
+
+use App\Containers\Vendor\Configurationer\Tasks\CreateConfigurationTask;
+use App\Containers\Vendor\Configurationer\Tasks\GetHostConfigurationTask;
+use Illuminate\Console\Command;
+
+class CreateHostConfigurationCommand extends Command
+{
+    protected $description = 'Generate host configuration';
+
+    protected $name = 'larabeans:host:configuration';
+
+    public function handle()
+    {
+        $configuration = app(GetHostConfigurationTask::class)->run();
+        if (!empty($configuration)) {
+            $this->line("<fg=red>" . "Configuration already exist.");
+        } else {
+            empty(app(CreateConfigurationTask::class)->run(['configurable_type' => 'host', 'configuration' => []])) ?: $this->line("<fg=green>" . "Configuration created sucessfully");
+        }
+    }
+}
