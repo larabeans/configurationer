13f91c113030aeb3fc5c217db22c5ded6df21b61
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-05 10:23:55 +0500

    Add listener for TenanterRegisterEvent to create tenant configurations

---
+++ Listeners/CreateTenantConfigurationListerner.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Containers\Vendor\Configurationer\Listeners;
+
+use App\Containers\Vendor\Configurationer\Tasks\CreateConfigurationTask;
+use App\Containers\Vendor\Tenanter\Events\TenantRegisteredEvent;
+
+
+class CreateTenantConfigurationListerner
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\OrderShipped  $event
+     * @return void
+     */
+    public function handle(TenantRegisteredEvent $event)
+    {
+
+        app(CreateConfigurationTask::class)->run(['configurable_type'=>'tenant','configuration'=>[],'tenant_id'=>$event->entity->id]);
+
+        // Access the order using $event->order...
+    }
+}
