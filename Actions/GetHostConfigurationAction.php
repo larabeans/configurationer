ced0f40944c3618bc01a5ac632acbe462b5721fb
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-05 18:22:09 +0500

    adds new action file to get host configuration

---
+++ Actions/GetHostConfigurationAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Containers\Vendor\Configurationer\Actions;
+
+use App\Containers\Vendor\Configurationer\Tasks\GetHostConfigurationTask;
+use App\Ship\Parents\Actions\Action;
+use App\Ship\Parents\Requests\Request;
+
+class GetHostConfigurationAction extends Action
+{
+    public function run(Request $request)
+    {
+        return app(GetHostConfigurationTask::class)->run();
+    }
+}
