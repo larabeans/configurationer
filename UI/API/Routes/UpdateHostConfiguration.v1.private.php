dc70f6aa6657d318885a39bfc7f3c5df4b555c80
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-05 18:57:48 +0500

    adds route to update host configuration

---
+++ UI/API/Routes/UpdateHostConfiguration.v1.private.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @apiGroup           Configuration
+ * @apiName            updateHostConfiguration
+ *
+ * @api                {PUT} /v1/configuration/host Update Host Configuration
+ * @apiDescription     Update host configuration
+ *
+ * @apiVersion         1.0.0
+ * @apiPermission      Authenticated User,admin
+ *
+ * @apiParam           {array}  configuration
+ *
+ * @apiSuccessExample  {json}  Success-Response:
+ * HTTP/1.1 200 OK
+{
+  // Insert the response of the request here...
+}
+ */
+
+use App\Containers\Vendor\Configurationer\UI\API\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+
+Route::put('configuration/host', [Controller::class, 'updateHostConfiguration'])
+    ->name('api_configurationer_update_host_configuration')
+    ->middleware(['auth:api']);
+
