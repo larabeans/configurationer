865778871e3d1e0eeed73e8992672fdfbee79fb2
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-12 18:56:50 +0500

    manages user,tenant,host configurations in a task

--- Tasks/UpdateConfigurationTask.php
+++ Tasks/UpdateConfigurationTask.php
@@ -3,6 +3,7 @@
 namespace App\Containers\Vendor\Configurationer\Tasks;
 
 use App\Containers\Vendor\Configurationer\Data\Repositories\ConfigurationRepository;
+use App\Containers\Vendor\Configurationer\Data\Repositories\ConfigurationHistoryRepository;
 use App\Ship\Exceptions\NotFoundException;
 use App\Ship\Exceptions\UpdateResourceFailedException;
 use App\Ship\Parents\Tasks\Task;
@@ -13,36 +14,58 @@ use App\Containers\Vendor\Configurationer\Tasks\CreateConfigurationHistoryTask;
 class UpdateConfigurationTask extends Task
 {
     protected ConfigurationRepository $repository;
+    protected ConfigurationHistoryRepository $historyRepository;
 
-    public function __construct(ConfigurationRepository $repository)
+    public function __construct(ConfigurationRepository $repository, ConfigurationHistoryRepository $historyRepository)
     {
         $this->repository = $repository;
+        $this->historyRepository = $historyRepository;
     }
 
     public function run($type, array $data)
     {
         $configurable_id = null;
+        $configuration =null;
         try {
             if ($type == "user") {
                 $configurable_id = Auth::user()->id;
+                $configuration= $this->getConfiguration($configurable_id);
 
             } else if ($type == "tenant") {
                 $configurable_id = Auth::user()->tenant_id;
+                $configuration= $this->getConfiguration($configurable_id);
+
+            } else if ($type == "host") {
+                $configuration = $this->repository->findWhere([
+                    'tenant_id' => null,
+                    'configurable_id' => ''
+                ])->first();
+
             }
-            $configuration = $this->repository->where('configurable_id', $configurable_id)->first();
 
-            if (!$configuration) {
 
-                throw new NotFoundException();
-            }
+            $historyData = [
+                "configuration_id" => $configuration->id,
+                "configuration" => $configuration->configuration
+            ];
+            $history = $this->historyRepository->create($historyData);
 
-            $history = app(CreateConfigurationHistoryTask::class)->run([$configuration->id, json_encode($configuration->configuration)]);
             $d = [
-                'configuration' => json_encode($data['configuration'])
+                'configuration' => $data['configuration']
             ];
             return $this->repository->update($d, $configuration->id);
         } catch (Exception $exception) {
             throw new UpdateResourceFailedException($exception);
         }
+    }
+
+    private function getConfiguration($id){
+        $configuration = $this->repository->where('configurable_id', $id)->first();
+
+        if (!$configuration) {
+
+            throw new NotFoundException("Configuration not found");
+        }
+        return $configuration;
     }
 }
