1c85b90bd4ee469f9d7d7367151e2737e0aa004d
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-16 16:34:53 +0500

    changes uri

--- UI/API/Routes/GetDefaultConfiguration.v1.public.php
+++ UI/API/Routes/GetDefaultConfiguration.v1.public.php
@@ -4,7 +4,7 @@
  * @apiGroup           Configuration
  * @apiName            defaultConfiguration
  *
- * @api                {GET} /v1/configurations Get Default Configurations
+ * @api                {GET} /v1/configurations/default Get Default Configurations
  * @apiDescription     Return the default configurations, user specific configuration, tenant specific configuration
  *
  * @apiVersion         1.0.0
@@ -24,7 +24,7 @@
 use App\Containers\Vendor\Configurationer\UI\API\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('configurations', [Controller::class, 'defaultConfiguration'])
+Route::get('configurations/default', [Controller::class, 'defaultConfiguration'])
     ->name('api_configuration_default_configuration')
     ->middleware(['auth:api']);
 
