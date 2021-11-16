ed35aaf5d097c9436930a7e8f1cb666a33b9b3be
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-16 15:50:33 +0500

    adds IsHostTrait

---
+++ Traits/IsHostTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Containers\Vendor\Configurationer\Traits;
+
+use Illuminate\Support\Facades\Auth;
+
+trait IsHostTrait
+{
+    public function isHost(): bool
+    {
+        $user = Auth::user();
+        if (sizeof($user->roles) == 0) {
+            return false;
+        } else {
+            foreach ($user->roles as $role) {
+                if ($role->name == "admin") {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+}
