a6090cd913392d4068d2731d71554418ab8b8dc9
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-17 14:34:36 +0500

    changes file name IsHostTrait to IsHostAdminTrait

--- Traits/IsHostAdminTrait.php
+++ Traits/IsHostAdminTrait.php
@@ -4,7 +4,7 @@ namespace App\Containers\Vendor\Configurationer\Traits;
 
 use Illuminate\Support\Facades\Auth;
 
-trait IsHostTrait
+trait IsHostAdminTrait
 {
     public function isHost(): bool
     {
