9b96c8b9a9376715d784e70425ed277664b5c125
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-18 18:41:19 +0500

    add config file to hold default configuration data

---
+++ Configs/default-tenant-configuration.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'configuration' => [
+        "register_user_in_system" => true,
+        "new_user_active_by_default" => false,
+        "captcha_on_registration" => false,
+        "captcha_on_login" => false,
+        "enabled_cookie_consent" => false,
+        "enabled_session_timeout" => false,
+        "email_confirmation_for_login" => false,
+        "allow_profile_picture" => true,
+        "user_defaultsettings" => false,
+        "require_digit" => true,
+        "require_lowercase" => false,
+        "require_non_alphanumeric" => false,
+        "require_uppercase" => true,
+        "password_lenght" => 1,
+        "user_account_locking" => false,
+        "number_of_login_attemps" => 2,
+        "account_locking_duration" => 3,
+        "name" => null,
+        "address" => null,
+        "tax_number" => null
+    ]
+];
