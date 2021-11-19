45f6d3bd736358b08ad615ec8d1d050dcde43c3d
Sajjad Naqvi <sajjad@orenda.systems>
2021-11-19 18:09:46 +0500

    updates file to change configuration data

--- Configs/default-tenant-configuration.php
+++ Configs/default-tenant-configuration.php
@@ -2,25 +2,39 @@
 
 return [
     'configuration' => [
-        "register_user_in_system" => true,
-        "new_user_active_by_default" => false,
-        "captcha_on_registration" => false,
-        "captcha_on_login" => false,
-        "enabled_cookie_consent" => false,
-        "enabled_session_timeout" => false,
-        "email_confirmation_for_login" => false,
-        "allow_profile_picture" => true,
-        "user_defaultsettings" => false,
-        "require_digit" => true,
-        "require_lowercase" => false,
-        "require_non_alphanumeric" => false,
-        "require_uppercase" => true,
-        "password_lenght" => 1,
-        "user_account_locking" => false,
-        "number_of_login_attemps" => 2,
-        "account_locking_duration" => 3,
-        "name" => null,
-        "address" => null,
-        "tax_number" => null
+        "appearance" => [
+            "animation_logo" => null,
+            "style" => null
+        ],
+        "user_management" => [
+            "register_user_in_system" => true,
+            "new_user_active_by_default" => true,
+            "captcha_on_registration" => true,
+            "captcha_on_login" => true,
+            "enabled_cookie_consent" => true,
+            "enabled_session_timeout" => false,
+            "email_confirmation_for_login" => false,
+            "allow_profile_picture" => true
+        ],
+        "security" => [
+            "user_defaultsettings" => true,
+            "require_digit" => false,
+            "require_lowercase" => true,
+            "require_non_alphanumeric" => false,
+            "require_uppercase" => true,
+            "password_lenght" => 111,
+            "user_account_locking" => true,
+            "number_of_login_attemps" => 222,
+            "account_locking_duration" => 333
+        ],
+        "invoice" => [
+            "name" => "Legal Name",
+            "address" => "VPO",
+            "tax_number" => "313133"
+        ],
+        "time_zone" => [
+            "time_zone_id" => "timeeeee",
+            "clock_provider" => "clock"
+        ]
     ]
 ];
